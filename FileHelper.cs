ed18915d09dd87d6d50858b1ee63bcc1af04499d
Soumya Sutar <soumsutar9@gmail.com>
2023-12-06 19:11:22 +0530

    Teacher_Record

---
+++ FileHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Record
+{
+    public class FileHelper
+    {
+        private const string FilePath = "C:\\Users\\soums\\OneDrive\\Desktop\\Simplilear\\Teachers.txt";
+
+        public static void SaveTeachers(List<Teacher> teachers)
+        {
+            var lines = teachers.Select(t => $"{t.ID},{t.Name},{t.ClassAndSection}");
+            File.WriteAllLines(FilePath, lines);
+        }
+        public static List<Teacher> LoadTeachers()
+        {
+            var teachers = new List<Teacher>();
+            if (File.Exists(FilePath))
+            {
+                var lines = File.ReadAllLines(FilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 3 && int.TryParse(parts[0], out int id))
+                    {
+                        teachers.Add(new Teacher
+                        {
+                            ID = id,
+                            Name = parts[1],
+                            ClassAndSection = parts[2]
+                        });
+                    }
+                }
+            }
+            return teachers;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Record
+{
+    internal class Program
+    {
+        static List<Teacher> teachers;
+        static void Main()
+        {
+            teachers = FileHelper.LoadTeachers();
+            while (true)
+            {
+                Console.WriteLine("Teacher Records System");
+                Console.WriteLine("1. Add Teacher");
+                Console.WriteLine("2. Update Teacher");
+                Console.WriteLine("3. Display All Teachers");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            AddTeacher();
+                            break;
+                        case 2:
+                            UpdateTeacher();
+                            break;
+                        case 3:
+                            DisplayAllTeachers();
+                            break;
+                        case 4:
+                            FileHelper.SaveTeachers(teachers);
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void AddTeacher()
+        {
+            Console.WriteLine("Adding a New Teacher");
+            Console.Write("Enter Teacher ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            // Add input validation for unique ID
+
+            Console.Write("Enter Teacher Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Class and Section: ");
+            string classAndSection = Console.ReadLine();
+
+            teachers.Add(new Teacher { ID = id, Name = name, ClassAndSection = classAndSection });
+            Console.WriteLine("Teacher added successfully.");
+        }
+
+        static void UpdateTeacher()
+        {
+            Console.WriteLine("Updating Teacher");
+            Console.Write("Enter Teacher ID to update: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Teacher teacherToUpdate = teachers.Find(t => t.ID == id);
+
+            if (teacherToUpdate != null)
+            {
+                Console.Write("Enter new Teacher Name: ");
+                teacherToUpdate.Name = Console.ReadLine();
+
+                Console.Write("Enter new Class and Section: ");
+                teacherToUpdate.ClassAndSection = Console.ReadLine();
+
+                Console.WriteLine("Teacher updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Teacher with ID {id} not found.");
+            }
+        }
+
+        static void DisplayAllTeachers()
+        {
+            Console.WriteLine("All Teachers:");
+
+            foreach (var teacher in teachers)
+            {
+                Console.WriteLine($"ID: {teacher.ID}, Name: {teacher.Name}, Class and Section: {teacher.ClassAndSection}");
+            }
+        }
+    }
+}
